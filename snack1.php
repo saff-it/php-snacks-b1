761c317b1ef2049767528b93940aabd20d769783
saff-it <saff_it@ymail.com>
2022-08-23 16:46:15 +0200

    Snack 5 - php set up + array creation

--- snack1.php
+++ snack1.php
@@ -1,36 +1,36 @@
 <?php
-$array = [
-    [
-        'squadra di casa' => 'Inter',
-        'squadra ospite' => 'Milan',
-        'punteggio squadra di casa' => '4',
-        'punteggio squadra ospite' => '0',
-    ],
-    [
-        'squadra di casa' => 'Inter',
-        'squadra ospite' => 'Roma',
-        'punteggio squadra di casa' => '3',
-        'punteggio squadra ospite' => '0',
-    ],
-    [
-        'squadra di casa' => 'Inter',
-        'squadra ospite' => 'Atalanta',
-        'punteggio squadra di casa' => '1',
-        'punteggio squadra ospite' => '1',
-    ],
-    [
-        'squadra di casa' => 'Inter',
-        'squadra ospite' => 'Lazio',
-        'punteggio squadra di casa' => '2',
-        'punteggio squadra ospite' => '1',
-    ],
-    [
-        'squadra di casa' => 'Inter',
-        'squadra ospite' => 'Sassuolo',
-        'punteggio squadra di casa' => '0',
-        'punteggio squadra ospite' => '2',
-    ],
-];
+    $array = [
+        [
+            'squadra di casa' => 'Inter',
+            'squadra ospite' => 'Milan',
+            'punteggio squadra di casa' => '4',
+            'punteggio squadra ospite' => '0',
+        ],
+        [
+            'squadra di casa' => 'Inter',
+            'squadra ospite' => 'Roma',
+            'punteggio squadra di casa' => '3',
+            'punteggio squadra ospite' => '0',
+        ],
+        [
+            'squadra di casa' => 'Inter',
+            'squadra ospite' => 'Atalanta',
+            'punteggio squadra di casa' => '1',
+            'punteggio squadra ospite' => '1',
+        ],
+        [
+            'squadra di casa' => 'Inter',
+            'squadra ospite' => 'Lazio',
+            'punteggio squadra di casa' => '2',
+            'punteggio squadra ospite' => '1',
+        ],
+        [
+            'squadra di casa' => 'Inter',
+            'squadra ospite' => 'Sassuolo',
+            'punteggio squadra di casa' => '0',
+            'punteggio squadra ospite' => '2',
+        ],
+    ];
 ?>
 
 <!DOCTYPE html>
--- snack5.php
+++ snack5.php
@@ -1,12 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Snack 5</title>
 </head>
+
 <body>
-    
+
+    <?php
+        $classroom = [
+            [
+                'Nome' => 'Simone',
+                'Cognome' => 'Saffiotti',
+                    [   
+                        "Matematica" => '8',
+                        "Storia" => '7',
+                        "Latino" => '9',
+                        "informatica" => '10',
+                    ],
+            ],
+            [
+                'Nome' => 'Mario',
+                'Cognome' => 'Rossi',
+                    [   
+                        "Matematica" => '4',
+                        "Storia" => '7',
+                        "Latino" => '2',
+                        "informatica" => '9',
+                    ],
+            ],
+            [
+                'Nome' => 'Maria',
+                'Cognome' => 'Bianchi',
+                    [   
+                        "Matematica" => '7',
+                        "Storia" => '3',
+                        "Latino" => '9',
+                        "informatica" => '1',
+                    ],
+            ],
+            [
+                'Nome' => 'Paolo',
+                'Cognome' => 'Paoli',
+                    [   
+                        "Matematica" => '10',
+                        "Storia" => '10',
+                        "Latino" => '2',
+                        "informatica" => '4',
+                    ],
+            ],
+            
+        ];
+
+        // var_dump($classroom);
+
+        for ($i = 0; $i < count($array); $i++) {
+    ?>
+
+        <h3>
+            <?php 
+                echo 
+            ?>
+        </h3>
+
+    <?php 
+        }
+    ?>
+
+
 </body>
+
 </html>
